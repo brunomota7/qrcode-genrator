391075df20951cf3b8e3d1c704c9468526527b92
Bruno Mota <bruno7motadev@gmail.com>
2025-04-28 20:40:30 -0300

    feat: Finished the controller QrCodeGenerate

--- src/main/java/com/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.qrcode.generator.controller;
 
 import com.qrcode.generator.dto.qrcodeDTO.QrCodeGenerateRequest;
 import com.qrcode.generator.dto.qrcodeDTO.QrCodeGenerateResponse;
+import com.qrcode.generator.service.QrCodeGenerateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,9 +10,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
-    @PostMapping
-    public ResponseEntity<QrCodeGenerateResponse> genarate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+    private final QrCodeGenerateService qrCodeGenerateService;
+
+    public QrCodeController(QrCodeGenerateService qrCodeService) {
+        this.qrCodeGenerateService = qrCodeService;
     }
 
+    @PostMapping
+    public ResponseEntity<QrCodeGenerateResponse> genarate(@RequestBody QrCodeGenerateRequest request) {
+        try {
+            QrCodeGenerateResponse response = qrCodeGenerateService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
